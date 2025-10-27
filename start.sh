740e297dddc2dff08eb89dcf084d7a2f6e44a3bc
Jitesh Kalta <jiteshkalta@gmail.com>
2025-10-28 04:54:19 +0530

    deployment changes

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e  # Exit immediately if a command fails
-
-cd backend
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Run FastAPI
-uvicorn main:app --host 0.0.0.0 --port $PORT
